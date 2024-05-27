198824c3bfd9b0fa358ec4cb3d6342f56311c789
th314159 <165121452+th314159@users.noreply.github.com>
2024-05-27 16:24:25 -0700

    Add files via upload

---
+++ build_new/test/conv_tensor_rearrange/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /work1/sadasivan/student30/composable_kernel/test/conv_tensor_rearrange
+# Build directory: /work1/sadasivan/student30/composable_kernel/build_new/test/conv_tensor_rearrange
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_conv_tensor_rearrange "/work1/sadasivan/student30/composable_kernel/build_new/bin/test_conv_tensor_rearrange")
+set_tests_properties(test_conv_tensor_rearrange PROPERTIES  _BACKTRACE_TRIPLES "/work1/sadasivan/student30/composable_kernel/test/CMakeLists.txt;113;add_test;/work1/sadasivan/student30/composable_kernel/test/conv_tensor_rearrange/CMakeLists.txt;1;add_gtest_executable;/work1/sadasivan/student30/composable_kernel/test/conv_tensor_rearrange/CMakeLists.txt;0;")
+add_test(test_conv_tensor_rearrange_interface "/work1/sadasivan/student30/composable_kernel/build_new/bin/test_conv_tensor_rearrange_interface")
+set_tests_properties(test_conv_tensor_rearrange_interface PROPERTIES  _BACKTRACE_TRIPLES "/work1/sadasivan/student30/composable_kernel/test/CMakeLists.txt;113;add_test;/work1/sadasivan/student30/composable_kernel/test/conv_tensor_rearrange/CMakeLists.txt;4;add_gtest_executable;/work1/sadasivan/student30/composable_kernel/test/conv_tensor_rearrange/CMakeLists.txt;0;")
