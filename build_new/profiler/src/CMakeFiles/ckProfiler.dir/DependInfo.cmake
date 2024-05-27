51ab6a4271cbe448588e7db9ac5b79d8eaa0ac26
th314159 <165121452+th314159@users.noreply.github.com>
2024-05-27 15:48:37 -0700

    Add files via upload

---
+++ build_new/profiler/src/CMakeFiles/ckProfiler.dir/DependInfo.cmake
@@ -0,0 +1,58 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/work1/sadasivan/student30/composable_kernel/profiler/src/profile_avg_pool3d_bwd.cpp" "profiler/src/CMakeFiles/ckProfiler.dir/profile_avg_pool3d_bwd.cpp.o" "gcc" "profiler/src/CMakeFiles/ckProfiler.dir/profile_avg_pool3d_bwd.cpp.o.d"
+  "/work1/sadasivan/student30/composable_kernel/profiler/src/profile_batched_gemm.cpp" "profiler/src/CMakeFiles/ckProfiler.dir/profile_batched_gemm.cpp.o" "gcc" "profiler/src/CMakeFiles/ckProfiler.dir/profile_batched_gemm.cpp.o.d"
+  "/work1/sadasivan/student30/composable_kernel/profiler/src/profile_batched_gemm_add_relu_gemm_add.cpp" "profiler/src/CMakeFiles/ckProfiler.dir/profile_batched_gemm_add_relu_gemm_add.cpp.o" "gcc" "profiler/src/CMakeFiles/ckProfiler.dir/profile_batched_gemm_add_relu_gemm_add.cpp.o.d"
+  "/work1/sadasivan/student30/composable_kernel/profiler/src/profile_batched_gemm_gemm.cpp" "profiler/src/CMakeFiles/ckProfiler.dir/profile_batched_gemm_gemm.cpp.o" "gcc" "profiler/src/CMakeFiles/ckProfiler.dir/profile_batched_gemm_gemm.cpp.o.d"
+  "/work1/sadasivan/student30/composable_kernel/profiler/src/profile_batched_gemm_reduce.cpp" "profiler/src/CMakeFiles/ckProfiler.dir/profile_batched_gemm_reduce.cpp.o" "gcc" "profiler/src/CMakeFiles/ckProfiler.dir/profile_batched_gemm_reduce.cpp.o.d"
+  "/work1/sadasivan/student30/composable_kernel/profiler/src/profile_batchnorm_bwd.cpp" "profiler/src/CMakeFiles/ckProfiler.dir/profile_batchnorm_bwd.cpp.o" "gcc" "profiler/src/CMakeFiles/ckProfiler.dir/profile_batchnorm_bwd.cpp.o.d"
+  "/work1/sadasivan/student30/composable_kernel/profiler/src/profile_batchnorm_fwd.cpp" "profiler/src/CMakeFiles/ckProfiler.dir/profile_batchnorm_fwd.cpp.o" "gcc" "profiler/src/CMakeFiles/ckProfiler.dir/profile_batchnorm_fwd.cpp.o.d"
+  "/work1/sadasivan/student30/composable_kernel/profiler/src/profile_batchnorm_infer.cpp" "profiler/src/CMakeFiles/ckProfiler.dir/profile_batchnorm_infer.cpp.o" "gcc" "profiler/src/CMakeFiles/ckProfiler.dir/profile_batchnorm_infer.cpp.o.d"
+  "/work1/sadasivan/student30/composable_kernel/profiler/src/profile_contraction_bilinear.cpp" "profiler/src/CMakeFiles/ckProfiler.dir/profile_contraction_bilinear.cpp.o" "gcc" "profiler/src/CMakeFiles/ckProfiler.dir/profile_contraction_bilinear.cpp.o.d"
+  "/work1/sadasivan/student30/composable_kernel/profiler/src/profile_contraction_scale.cpp" "profiler/src/CMakeFiles/ckProfiler.dir/profile_contraction_scale.cpp.o" "gcc" "profiler/src/CMakeFiles/ckProfiler.dir/profile_contraction_scale.cpp.o.d"
+  "/work1/sadasivan/student30/composable_kernel/profiler/src/profile_conv_bwd_data.cpp" "profiler/src/CMakeFiles/ckProfiler.dir/profile_conv_bwd_data.cpp.o" "gcc" "profiler/src/CMakeFiles/ckProfiler.dir/profile_conv_bwd_data.cpp.o.d"
+  "/work1/sadasivan/student30/composable_kernel/profiler/src/profile_conv_fwd.cpp" "profiler/src/CMakeFiles/ckProfiler.dir/profile_conv_fwd.cpp.o" "gcc" "profiler/src/CMakeFiles/ckProfiler.dir/profile_conv_fwd.cpp.o.d"
+  "/work1/sadasivan/student30/composable_kernel/profiler/src/profile_conv_fwd_bias_relu.cpp" "profiler/src/CMakeFiles/ckProfiler.dir/profile_conv_fwd_bias_relu.cpp.o" "gcc" "profiler/src/CMakeFiles/ckProfiler.dir/profile_conv_fwd_bias_relu.cpp.o.d"
+  "/work1/sadasivan/student30/composable_kernel/profiler/src/profile_conv_fwd_bias_relu_add.cpp" "profiler/src/CMakeFiles/ckProfiler.dir/profile_conv_fwd_bias_relu_add.cpp.o" "gcc" "profiler/src/CMakeFiles/ckProfiler.dir/profile_conv_fwd_bias_relu_add.cpp.o.d"
+  "/work1/sadasivan/student30/composable_kernel/profiler/src/profile_conv_tensor_rearrange.cpp" "profiler/src/CMakeFiles/ckProfiler.dir/profile_conv_tensor_rearrange.cpp.o" "gcc" "profiler/src/CMakeFiles/ckProfiler.dir/profile_conv_tensor_rearrange.cpp.o.d"
+  "/work1/sadasivan/student30/composable_kernel/profiler/src/profile_gemm.cpp" "profiler/src/CMakeFiles/ckProfiler.dir/profile_gemm.cpp.o" "gcc" "profiler/src/CMakeFiles/ckProfiler.dir/profile_gemm.cpp.o.d"
+  "/work1/sadasivan/student30/composable_kernel/profiler/src/profile_gemm_add_add_fastgelu.cpp" "profiler/src/CMakeFiles/ckProfiler.dir/profile_gemm_add_add_fastgelu.cpp.o" "gcc" "profiler/src/CMakeFiles/ckProfiler.dir/profile_gemm_add_add_fastgelu.cpp.o.d"
+  "/work1/sadasivan/student30/composable_kernel/profiler/src/profile_gemm_add_fastgelu.cpp" "profiler/src/CMakeFiles/ckProfiler.dir/profile_gemm_add_fastgelu.cpp.o" "gcc" "profiler/src/CMakeFiles/ckProfiler.dir/profile_gemm_add_fastgelu.cpp.o.d"
+  "/work1/sadasivan/student30/composable_kernel/profiler/src/profile_gemm_add_multiply.cpp" "profiler/src/CMakeFiles/ckProfiler.dir/profile_gemm_add_multiply.cpp.o" "gcc" "profiler/src/CMakeFiles/ckProfiler.dir/profile_gemm_add_multiply.cpp.o.d"
+  "/work1/sadasivan/student30/composable_kernel/profiler/src/profile_gemm_add_relu_add_layernorm.cpp" "profiler/src/CMakeFiles/ckProfiler.dir/profile_gemm_add_relu_add_layernorm.cpp.o" "gcc" "profiler/src/CMakeFiles/ckProfiler.dir/profile_gemm_add_relu_add_layernorm.cpp.o.d"
+  "/work1/sadasivan/student30/composable_kernel/profiler/src/profile_gemm_bias_add_reduce.cpp" "profiler/src/CMakeFiles/ckProfiler.dir/profile_gemm_bias_add_reduce.cpp.o" "gcc" "profiler/src/CMakeFiles/ckProfiler.dir/profile_gemm_bias_add_reduce.cpp.o.d"
+  "/work1/sadasivan/student30/composable_kernel/profiler/src/profile_gemm_bilinear.cpp" "profiler/src/CMakeFiles/ckProfiler.dir/profile_gemm_bilinear.cpp.o" "gcc" "profiler/src/CMakeFiles/ckProfiler.dir/profile_gemm_bilinear.cpp.o.d"
+  "/work1/sadasivan/student30/composable_kernel/profiler/src/profile_gemm_fastgelu.cpp" "profiler/src/CMakeFiles/ckProfiler.dir/profile_gemm_fastgelu.cpp.o" "gcc" "profiler/src/CMakeFiles/ckProfiler.dir/profile_gemm_fastgelu.cpp.o.d"
+  "/work1/sadasivan/student30/composable_kernel/profiler/src/profile_gemm_multiply_add.cpp" "profiler/src/CMakeFiles/ckProfiler.dir/profile_gemm_multiply_add.cpp.o" "gcc" "profiler/src/CMakeFiles/ckProfiler.dir/profile_gemm_multiply_add.cpp.o.d"
+  "/work1/sadasivan/student30/composable_kernel/profiler/src/profile_gemm_reduce.cpp" "profiler/src/CMakeFiles/ckProfiler.dir/profile_gemm_reduce.cpp.o" "gcc" "profiler/src/CMakeFiles/ckProfiler.dir/profile_gemm_reduce.cpp.o.d"
+  "/work1/sadasivan/student30/composable_kernel/profiler/src/profile_gemm_splitk.cpp" "profiler/src/CMakeFiles/ckProfiler.dir/profile_gemm_splitk.cpp.o" "gcc" "profiler/src/CMakeFiles/ckProfiler.dir/profile_gemm_splitk.cpp.o.d"
+  "/work1/sadasivan/student30/composable_kernel/profiler/src/profile_gemm_streamk.cpp" "profiler/src/CMakeFiles/ckProfiler.dir/profile_gemm_streamk.cpp.o" "gcc" "profiler/src/CMakeFiles/ckProfiler.dir/profile_gemm_streamk.cpp.o.d"
+  "/work1/sadasivan/student30/composable_kernel/profiler/src/profile_grouped_conv_bwd_data.cpp" "profiler/src/CMakeFiles/ckProfiler.dir/profile_grouped_conv_bwd_data.cpp.o" "gcc" "profiler/src/CMakeFiles/ckProfiler.dir/profile_grouped_conv_bwd_data.cpp.o.d"
+  "/work1/sadasivan/student30/composable_kernel/profiler/src/profile_grouped_conv_bwd_weight.cpp" "profiler/src/CMakeFiles/ckProfiler.dir/profile_grouped_conv_bwd_weight.cpp.o" "gcc" "profiler/src/CMakeFiles/ckProfiler.dir/profile_grouped_conv_bwd_weight.cpp.o.d"
+  "/work1/sadasivan/student30/composable_kernel/profiler/src/profile_grouped_conv_fwd.cpp" "profiler/src/CMakeFiles/ckProfiler.dir/profile_grouped_conv_fwd.cpp.o" "gcc" "profiler/src/CMakeFiles/ckProfiler.dir/profile_grouped_conv_fwd.cpp.o.d"
+  "/work1/sadasivan/student30/composable_kernel/profiler/src/profile_grouped_gemm.cpp" "profiler/src/CMakeFiles/ckProfiler.dir/profile_grouped_gemm.cpp.o" "gcc" "profiler/src/CMakeFiles/ckProfiler.dir/profile_grouped_gemm.cpp.o.d"
+  "/work1/sadasivan/student30/composable_kernel/profiler/src/profile_grouped_gemm_fastgelu.cpp" "profiler/src/CMakeFiles/ckProfiler.dir/profile_grouped_gemm_fastgelu.cpp.o" "gcc" "profiler/src/CMakeFiles/ckProfiler.dir/profile_grouped_gemm_fastgelu.cpp.o.d"
+  "/work1/sadasivan/student30/composable_kernel/profiler/src/profile_groupnorm.cpp" "profiler/src/CMakeFiles/ckProfiler.dir/profile_groupnorm.cpp.o" "gcc" "profiler/src/CMakeFiles/ckProfiler.dir/profile_groupnorm.cpp.o.d"
+  "/work1/sadasivan/student30/composable_kernel/profiler/src/profile_layernorm.cpp" "profiler/src/CMakeFiles/ckProfiler.dir/profile_layernorm.cpp.o" "gcc" "profiler/src/CMakeFiles/ckProfiler.dir/profile_layernorm.cpp.o.d"
+  "/work1/sadasivan/student30/composable_kernel/profiler/src/profile_max_pool3d_bwd.cpp" "profiler/src/CMakeFiles/ckProfiler.dir/profile_max_pool3d_bwd.cpp.o" "gcc" "profiler/src/CMakeFiles/ckProfiler.dir/profile_max_pool3d_bwd.cpp.o.d"
+  "/work1/sadasivan/student30/composable_kernel/profiler/src/profile_max_pool3d_fwd.cpp" "profiler/src/CMakeFiles/ckProfiler.dir/profile_max_pool3d_fwd.cpp.o" "gcc" "profiler/src/CMakeFiles/ckProfiler.dir/profile_max_pool3d_fwd.cpp.o.d"
+  "/work1/sadasivan/student30/composable_kernel/profiler/src/profile_reduce.cpp" "profiler/src/CMakeFiles/ckProfiler.dir/profile_reduce.cpp.o" "gcc" "profiler/src/CMakeFiles/ckProfiler.dir/profile_reduce.cpp.o.d"
+  "/work1/sadasivan/student30/composable_kernel/profiler/src/profile_softmax.cpp" "profiler/src/CMakeFiles/ckProfiler.dir/profile_softmax.cpp.o" "gcc" "profiler/src/CMakeFiles/ckProfiler.dir/profile_softmax.cpp.o.d"
+  "/work1/sadasivan/student30/composable_kernel/profiler/src/profiler.cpp" "profiler/src/CMakeFiles/ckProfiler.dir/profiler.cpp.o" "gcc" "profiler/src/CMakeFiles/ckProfiler.dir/profiler.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/work1/sadasivan/student30/composable_kernel/build_new/library/src/utility/CMakeFiles/utility.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
