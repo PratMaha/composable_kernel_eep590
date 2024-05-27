dc6fa928324dc66e78727ba5d7284722bd5abac2
th314159 <165121452+th314159@users.noreply.github.com>
2024-05-27 16:26:11 -0700

    Add files via upload

---
+++ build_new/test/convnd_fwd/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /work1/sadasivan/student30/composable_kernel/test/convnd_fwd
+# Build directory: /work1/sadasivan/student30/composable_kernel/build_new/test/convnd_fwd
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_convnd_fwd "/work1/sadasivan/student30/composable_kernel/build_new/bin/test_convnd_fwd")
+set_tests_properties(test_convnd_fwd PROPERTIES  _BACKTRACE_TRIPLES "/work1/sadasivan/student30/composable_kernel/test/CMakeLists.txt;113;add_test;/work1/sadasivan/student30/composable_kernel/test/convnd_fwd/CMakeLists.txt;5;add_gtest_executable;/work1/sadasivan/student30/composable_kernel/test/convnd_fwd/CMakeLists.txt;0;")
