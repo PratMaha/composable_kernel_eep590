8c920366357f9f6be6b90b1197745937918d4e47
th314159 <165121452+th314159@users.noreply.github.com>
2024-05-27 16:32:11 -0700

    Add files via upload

---
+++ build_new/test/gemm_split_k/CMakeFiles/test_gemm_splitk.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../../bin/test_gemm_splitk"
+  "../../bin/test_gemm_splitk.pdb"
+  "CMakeFiles/test_gemm_splitk.dir/test_gemm_splitk.cpp.o"
+  "CMakeFiles/test_gemm_splitk.dir/test_gemm_splitk.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_gemm_splitk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
