16fefc4c72ae23d01d2bc0d770f99c990667cefd
th314159 <165121452+th314159@users.noreply.github.com>
2024-05-27 15:58:18 -0700

    Add files via upload

---
+++ build_new/_deps/googletest-build/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /work1/sadasivan/student30/composable_kernel/build_new/_deps/googletest-src/googlemock
+# Build directory: /work1/sadasivan/student30/composable_kernel/build_new/_deps/googletest-build/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
---
+++ build_new/_deps/googletest-build/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /work1/sadasivan/student30/composable_kernel/build_new/_deps/googletest-src/googletest
+# Build directory: /work1/sadasivan/student30/composable_kernel/build_new/_deps/googletest-build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
