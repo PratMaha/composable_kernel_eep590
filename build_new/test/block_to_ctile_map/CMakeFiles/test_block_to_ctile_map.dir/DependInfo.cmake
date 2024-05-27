a56a0c8750228508d53b4aaf7e35026dbefca1e1
th314159 <165121452+th314159@users.noreply.github.com>
2024-05-27 16:15:40 -0700

    Add files via upload

---
+++ build_new/test/block_to_ctile_map/CMakeFiles/test_block_to_ctile_map.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/work1/sadasivan/student30/composable_kernel/test/block_to_ctile_map/test_block_to_ctile_map.cpp" "test/block_to_ctile_map/CMakeFiles/test_block_to_ctile_map.dir/test_block_to_ctile_map.cpp.o" "gcc" "test/block_to_ctile_map/CMakeFiles/test_block_to_ctile_map.dir/test_block_to_ctile_map.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/work1/sadasivan/student30/composable_kernel/build_new/_deps/googletest-build/googletest/CMakeFiles/gtest_main.dir/DependInfo.cmake"
+  "/work1/sadasivan/student30/composable_kernel/build_new/_deps/googletest-build/googletest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build_new/test/block_to_ctile_map/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /work1/sadasivan/student30/composable_kernel/test/block_to_ctile_map
+# Build directory: /work1/sadasivan/student30/composable_kernel/build_new/test/block_to_ctile_map
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_block_to_ctile_map "/work1/sadasivan/student30/composable_kernel/build_new/bin/test_block_to_ctile_map")
+set_tests_properties(test_block_to_ctile_map PROPERTIES  _BACKTRACE_TRIPLES "/work1/sadasivan/student30/composable_kernel/test/CMakeLists.txt;113;add_test;/work1/sadasivan/student30/composable_kernel/test/block_to_ctile_map/CMakeLists.txt;1;add_gtest_executable;/work1/sadasivan/student30/composable_kernel/test/block_to_ctile_map/CMakeLists.txt;0;")
