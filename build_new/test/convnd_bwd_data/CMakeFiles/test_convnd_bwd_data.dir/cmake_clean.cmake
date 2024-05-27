797d79b91635ab6d5df1e3ef592190d5f85425b7
th314159 <165121452+th314159@users.noreply.github.com>
2024-05-27 16:25:25 -0700

    Add files via upload

---
+++ build_new/test/convnd_bwd_data/CMakeFiles/test_convnd_bwd_data.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../../bin/test_convnd_bwd_data"
+  "../../bin/test_convnd_bwd_data.pdb"
+  "CMakeFiles/test_convnd_bwd_data.dir/convnd_bwd_data.cpp.o"
+  "CMakeFiles/test_convnd_bwd_data.dir/convnd_bwd_data.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_convnd_bwd_data.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
