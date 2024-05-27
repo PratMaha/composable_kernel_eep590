7a3315faad316ccf6745965ebd85d4e56c7c8792
th314159 <165121452+th314159@users.noreply.github.com>
2024-05-27 16:23:44 -0700

    Add files via upload

---
+++ build_new/test/contraction/CMakeFiles/test_contraction_interface.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../../bin/test_contraction_interface"
+  "../../bin/test_contraction_interface.pdb"
+  "CMakeFiles/test_contraction_interface.dir/test_contraction_interface.cpp.o"
+  "CMakeFiles/test_contraction_interface.dir/test_contraction_interface.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_contraction_interface.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build_new/test/contraction/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /work1/sadasivan/student30/composable_kernel/test/contraction
+# Build directory: /work1/sadasivan/student30/composable_kernel/build_new/test/contraction
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_contraction "/work1/sadasivan/student30/composable_kernel/build_new/bin/test_contraction")
+set_tests_properties(test_contraction PROPERTIES  _BACKTRACE_TRIPLES "/work1/sadasivan/student30/composable_kernel/test/CMakeLists.txt;113;add_test;/work1/sadasivan/student30/composable_kernel/test/contraction/CMakeLists.txt;6;add_gtest_executable;/work1/sadasivan/student30/composable_kernel/test/contraction/CMakeLists.txt;0;")
+add_test(test_contraction_interface "/work1/sadasivan/student30/composable_kernel/build_new/bin/test_contraction_interface")
+set_tests_properties(test_contraction_interface PROPERTIES  _BACKTRACE_TRIPLES "/work1/sadasivan/student30/composable_kernel/test/CMakeLists.txt;113;add_test;/work1/sadasivan/student30/composable_kernel/test/contraction/CMakeLists.txt;8;add_gtest_executable;/work1/sadasivan/student30/composable_kernel/test/contraction/CMakeLists.txt;0;")
