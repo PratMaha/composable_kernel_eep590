76e7713fac9c4b172a9ab17c96e668fae463e042
th314159 <165121452+th314159@users.noreply.github.com>
2024-06-03 21:50:31 -0700

    Delete build_new/_deps directory

--- build_new/_deps/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /work1/sadasivan/student30/composable_kernel/build_new/_deps/googletest-src
-# Build directory: /work1/sadasivan/student30/composable_kernel/build_new/_deps/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- build_new/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /work1/sadasivan/student30/composable_kernel/build_new/_deps/googletest-src/googletest
-# Build directory: /work1/sadasivan/student30/composable_kernel/build_new/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
