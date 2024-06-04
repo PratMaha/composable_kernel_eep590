d5f7a59edfb8750db9e1d445653ccb3958967358
th314159 <165121452+th314159@users.noreply.github.com>
2024-06-03 21:51:38 -0700

    Delete build_new/profiler directory

--- build_new/profiler/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /work1/sadasivan/student30/composable_kernel/profiler/src
-# Build directory: /work1/sadasivan/student30/composable_kernel/build_new/profiler/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
